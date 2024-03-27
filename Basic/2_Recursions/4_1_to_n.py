f24e2bfc877cea44e854321e540f10e51eef84a5
Dhiyanesh-babu <cv.dhiyanesh@gmail.com>
2024-03-27 19:14:01 +0530

    'testing'

--- Basic/2_Recursions/4_1_to_n.py
+++ Basic/2_Recursions/4_1_to_n.py
@@ -1,12 +1,15 @@
 class n_to_1:
-    def __init__(self,data) -> None:
+    def __init__(self, data) -> None:
         self.num = data
+
     def show(self):
         if self.num == 0:
             return 0
-        n_to_1(self.num-1).show()
+        n_to_1(self.num - 1).show()
         print(self.num)
+
 
 sample = 5
 obj = n_to_1(sample)
 obj.show()
+## hi
